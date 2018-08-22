ac586e0856a961086c26c3c24c85e8f7368adc97
webpct <webpct@>
2018-08-22 18:18:03 +0300

    fs

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -22,87 +22,103 @@ import Map from '../Map/Map';
 class Order extends Component {
         constructor(props) {
             super(props);
-
-
-            this.state = {isMenuVisible: false, count: 7};
+            this.state = {
+                isMenuVisible: false,
+                count: 7,
+                isMapVisible: false,
+                startNumber: 0,
+                endNumber: 1,
+                handleFunction: null,
+                startPointSelectOption: null,
+            }
         }
 
 
         onChangeEndPoint(optionSelected,action) {
+            //this.state.isMapVisible
             this.props.dispatch(setOrderEndPoint(optionSelected.value));
         }
 
+        test = (text,position) => {
+            const test = {
+                value: position,
+                label: text
+            };
+
+            this.setState({
+                startPointSelectOption:  test,
+                isMapVisible: false,
+                handleFunction: null,
+            });
+
+            console.log(this.state,test);
+        };
 
         onChangeStartPoint(optionSelected,action) {
-           this.props.dispatch(setOrderStartPoint(optionSelected.value));
+           if (optionSelected.value === 'map'){
+                this.setState({
+
+                    isMapVisible: true,
+                    handleFunction: this.test,
+                })
+           }
         }
 
 
         render(){
         return(
-            <React.Fragment>
-
             <div>
-                   <div>
-                       <TopBar/>
+                <div>
+                    <TopBar/>
 
-                       <div className='order-wp'>
-                           <OrderInput  onChangeCallback={this.onChangeStartPoint.bind(this)} token={this.props.token} />
-                           <input className="order-wp entrance" type="text" placeholder="Подъезд" />
-                       </div>
+                    <div className='order-wp'>
+                        <OrderInput onChangeCallback={this.onChangeStartPoint.bind(this)}  token={this.props.token} defaultValue = {this.state.startPointSelectOption} />
+                        <input className="order-wp entrance" type="text" placeholder="Подъезд" />
+                    </div>
+
+                    <div className="order-wp">
+                        <OrderInput onChangeCallback={this.onChangeEndPoint.bind(this)}/>
+                    </div>
+
+                    <textarea className="comment-text" placeholder="Комментарий к заказу"></textarea>
+                    <div className="btn-wp">
+                        <button className="order-econom">
+                            Эконом
+                        </button>
+
+                        <button className="dop-services">
+                            Доп. услуги
+                        </button>
+                    </div>
+
+                    <select>
+                        <option value="Наличными">Наличными</option>
+                        <option value="Банковской картой">Банковской картой</option>
+                    </select>
+
+                    <div className="price">
+                        Расстояние / рекомендуемая стоимость
+                        <div className="price-count">{(this.props.order.endPoint != null && this.props.order.startPoint != null) ? '7р' : '--'}<span>{(this.props.order.endPoint != null && this.props.order.startPoint != null) ? '14км' : '--'}</span></div>
+                    </div>
+
+                    <div className={(this.props.order.endPoint !=null && this.props.order.startPoint !=null) ? 'numbers numbers-active' : 'menu-bg'}>
+                        <button className="minus" onClick={ ()=>{this.setState({count: --this.state.count}) } }>-1
+                        </button>
+                        <div className="count"><span>{(this.props.order.endPoint != null && this.props.order.startPoint != null) ? this.state.count : '0'} р</span></div>
+                        <button className="plus" onClick={ ()=>{this.setState({count: ++this.state.count})} }>+1
+                        </button>
+                    </div>
+
+                    <button className="to-order" onClick={()=>{ if (this.props.order.endPoint != null && this.props.order.startPoint != null){ this.props.dispatch(changeScreenAction(
+                        <Load />))} } } > Заказать
+                    </button>
+                </div>
 
 
-                       <div className="order-wp">
-                           <OrderInput onChangeCallback={this.onChangeEndPoint.bind(this)}/>
-                       </div>
-
-                               <textarea className="comment-text" placeholder="Комментарий к заказу"></textarea>
-                       <div className="btn-wp">
-                           <button className="order-econom">
-                               Эконом
-                           </button>
-
-                           <button className="dop-services">
-                               Доп. услуги
-                           </button>
-                       </div>
-
-                                   <select>
-                                       <option value="Наличными">Наличными</option>
-                                       <option value="Банковской картой">Банковской картой</option>
-                                   </select>
+                {this.state.isMapVisible && <Map handler={this.state.handleFunction} />}
 
 
-                               <div className="price">
-                                   Расстояние / рекомендуемая стоимость
-                                   <div className="price-count">{(this.props.order.endPoint != null && this.props.order.startPoint != null) ? '7р' : '--'}<span>{(this.props.order.endPoint != null && this.props.order.startPoint != null) ? '14км' : '--'}</span></div>
-                               </div>
-
-
-                       <div className={(this.props.order.endPoint != null && this.props.order.startPoint != null) ? 'numbers numbers-active' : 'menu-bg'}>
-                           <button className="minus" onClick={
-                               ()=>{this.setState({count: --this.state.count})
-                               }
-                           }>-1</button>
-                           <div className="count"><span>{(this.props.order.endPoint != null && this.props.order.startPoint != null) ? this.state.count : '0'} р</span></div>
-                           <button className="plus" onClick={
-                               ()=>{this.setState({count: ++this.state.count})}
-                           }>+1</button>
-                       </div>
-
-                               <button className="to-order" onClick={()=>{
-                                   if (this.props.order.endPoint != null && this.props.order.startPoint != null){
-                                       this.props.dispatch(changeScreenAction(<Load />))}
-                               }
-                               } >
-                                   Заказать
-                               </button>
-                           </div>
-                   </div>
-
-                <Map/>
-
-            </React.Fragment>
+            </div>
 
                 )
     }
--- src/components/OrderInput/OrderInput.js
+++ src/components/OrderInput/OrderInput.js
@@ -11,6 +11,7 @@ export default class OrderInput extends Component{
 
     constructor(props) {
         super(props);
+        console.log(this.props.defaultValue);
         this.getOptions = _.throttle(this.promiseOptions,300);
         this.constOptions = [{ value: 'map', label: 'Посмотреть на карте' },
             { value: 'strawberry', label: 'Любимое место (Работа)' }];
@@ -50,7 +51,7 @@ export default class OrderInput extends Component{
 
     render() {
         return (
-            <AsyncSelect classNamePrefix="react-select" onChange={this.props.onChangeCallback} className="order-input" cacheOptions defaultOptions={this.constOptions} loadOptions={this.getOptions.bind(this)} />
+            <AsyncSelect classNamePrefix="react-select"  defaultValue={this.props.defaultValue} onChange={this.props.onChangeCallback} className="order-input" cacheOptions defaultOptions={this.constOptions} loadOptions={this.getOptions.bind(this)} />
         );
     }
 }
